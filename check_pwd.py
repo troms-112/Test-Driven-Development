6b9169249a5badb5d2ed4f9c0f5c8b2fb9c63e0a
Calvin Trombley <tromblec@oregonstate.edu>
2025-02-13 11:16:08 -0600

    Added digit validation functionality to check_pwd using a Boolean flag, now passes test 6. Check_pwd meets length, lowercase, uppercase, and digit requirements.

--- check_pwd.py
+++ check_pwd.py
@@ -1,10 +1,12 @@
 def check_pwd(pwd: str) -> bool:
     if not (8 <= len(pwd) <= 20):
         return False
-    lower_case = upper_case = False
+    lower_case = upper_case = digit = False
     for char in pwd:
         if char.islower():
             lower_case = True
         elif char.isupper():
             upper_case = True
-    return lower_case and upper_case
+        elif char.isdigit():
+            digit = True
+    return lower_case and upper_case and digit
