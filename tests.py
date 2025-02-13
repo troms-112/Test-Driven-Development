b10ba21247f87280be744883646e779e4cbe10dd
Calvin Trombley <tromblec@oregonstate.edu>
2025-02-13 10:10:03 -0600

    Test 1 checks if an empty string is correctly rejected. Test failed.

--- tests.py
+++ tests.py
@@ -3,7 +3,13 @@ from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
+    def test1(self):
+        """
+        test to check if an empty string is correctly rejected
 
+        :return: False
+        """
+        self.assertFalse(check_pwd(""))
 
 if __name__ == "__main__":
     unittest.main()
